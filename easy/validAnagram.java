5f13b24ecf93082f4074bfc79a2bdd9b11db4d83
Anh Nguyen <anh1303052005@gmail.com>
2026-02-26 09:41:10 +0700

    26th feb commit

---
+++ easy/validAnagram.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (char c: s.toCharArray())
+        {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        for (char c: t.toCharArray())
+        {
+            if (!charCount.containsKey(c))
+            {
+                return false;
+            }
+            charCount.put(c, charCount.get(c) - 1);
+            if (charCount.get(c) == 0)
+            {
+                charCount.remove(c);
+            }
+        }
+        return charCount.isEmpty();
+    }
+}
