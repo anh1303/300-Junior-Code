33898c88f3916f44e336123909e0f31579dbd1b9
Anh Nguyen <anh1303052005@gmail.com>
2026-03-02 08:44:55 +0700

    maxSubArray + lca

---
+++ easy/maximumSubarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int num: nums)
+        {
+            currentSum += num;
+            if (currentSum < num)
+            {
+                currentSum = num;
+            }
+            maxSum = Math.max(maxSum, currentSum);
+            
+        }
+        return maxSum;
+    }
+}
