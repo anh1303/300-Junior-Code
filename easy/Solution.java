a18ceed624a5abdd77df95c490066c290035e391
Anh Nguyen <anh1303052005@gmail.com>
2026-03-05 13:48:41 +0700

    5th March commit

---
+++ easy/Solution.java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int result = n;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid) && mid < result)
+            {
+                result = mid;
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+}
