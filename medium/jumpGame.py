c83bf6dc5b0a627a915e90233fe96f98f50c7338
Anh Nguyen <anh1303052005@gmail.com>
2026-08-17 11:23:45 +0700

    17th August commit

---
+++ medium/jumpGame.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        max_reachable = 0
+        for i, jump in enumerate(nums):
+            if i > max_reachable:
+                return False
+            max_reachable = max(max_reachable, i + jump)
+        return True
